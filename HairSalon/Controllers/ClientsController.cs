d0fa038ec4b886c88c1daf28bb06e3e8c77f1a8f
Nataliya Zhuravleva <natalindria@gmail.com>
2021-01-02 15:53:47 -0800

    Add ClientsController

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,22 @@
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controllers
+  {
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.ToList();
+      return View(model);
+    }
+  }
+}
