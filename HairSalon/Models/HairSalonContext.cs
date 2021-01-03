c46f569d60fba7aa45a13a5f9ad5ff524f34fd5c
Nataliya Zhuravleva <natalindria@gmail.com>
2021-01-02 18:14:27 -0800

    Update context file with stylist and lazy loading

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,7 +4,12 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
+    public virtual DbSet<Stylist> Stylists { get; set; }
     public DbSet<Client> Clients { get; set; }
     public HairSalonContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
